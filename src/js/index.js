8942b0aa8b396c5aecf8628dcfd07a80ed821549
Rimjheem <57498795+MohammadMiah24@users.noreply.github.com>
2021-03-23 10:26:05 -0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 console.log("connected");
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const submitButton = document.getElementById('submit-btn')
