995e08457d409ab5e38fc7612c4e07fa0520a5e0
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-03-26 09:00:40 -0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,13 @@ startButton.addEventListener("click", function (e) {
   console.log(e.target);
   console.log(e);
 });
+
+const questions = [
+  {
+    questionContent: "What is shinier than diamonds in the sky?"
+  }
+  {
+    choices: "Not my future", "Maybe, diamonds?", "Mr.Henriques's head", "Mr. Whalen's head in a zoom meeting.", "Dwayne Johnson's head.",
+    
+  }
+];
