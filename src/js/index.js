8804f6557e60aa65b43069f95462ccb4837f58d3
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-04-06 19:29:20 -0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,90 @@
-/*const startButton = document.getElementById("#start-btn");
-startButton.addEventListener("click", function (e) {
-  console.log(e.target);
-  console.log(e);
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
 });
+
+function startGame() {
+  //console.log("Started");
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
-    questionContent: "What is shinier than diamonds in the sky?"
-  }
-  {
-    choices: "Not my future", "Maybe, diamonds?", "Mr.Henriques's head", "Mr. Whalen's head in a zoom meeting.", "Dwayne Johnson's head.",
-    
-  }
-];*/
-
-let count = 0;
-function i() {
-  choices = document.getElementById("question");
-  if (count < choices.length) choices[count++].style.display = "question";
-}
-
-const myQuestions = [
-  {
-    question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
-    },
-    correctAnswer: "d",
+    question: "What is shinier than diamonds in the sky?",
+    answers: [
+      { text: "Not my future", correct: true },
+      { text: "Diamonds?", correct: false },
+      { text: "Mr. Henriques's head", correct: false },
+      { text: "Mr. Whalen's head in a zoom meeting.", correct: false },
+    ],
   },
 ];
